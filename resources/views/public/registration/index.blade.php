1847d423467855391b6012d70d75d70b15390ef0
a3mqq2 <aishaaltery03@gmail.com>
2025-10-22 21:21:59 +0200

    UPDATES

--- resources/views/public/registration/index.blade.php
+++ resources/views/public/registration/index.blade.php
@@ -525,7 +525,7 @@
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    <form class="contact-form">
+                    <form class="contact-form" method="POST">
                         <div class="mb-3">
                             <label for="contact_name" class="form-label">
                                 <i class="ti ti-user me-1"></i>
